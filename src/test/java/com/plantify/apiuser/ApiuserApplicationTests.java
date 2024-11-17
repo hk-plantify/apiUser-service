01cfaea97b101bcff450d87ef959047b051c5bdc
seunzu <0123suh@gmail.com>
2024-11-17 23:48:24 +0900

    chore: 초기 세팅

---
+++ src/test/java/com/plantify/apiuser/ApiuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantify.apiuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
